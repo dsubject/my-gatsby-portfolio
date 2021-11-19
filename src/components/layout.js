4ac17c740a21978c22a2e7bd4402444eb0054d9d
dani <daniellesubject@gmail.com>
2021-11-19 17:52:35 -0500

    removing carbon calculator api

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,11 +20,9 @@ const Layout = ({ children }) => {
                 <title>Dani Subject</title>
                 <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300&family=Merriweather&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300&display=swap" rel="stylesheet"/>
-                <script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js" defer></script>
             </Helmet>
             {children}
             <Footer />
-            <div style={{padding: 20, display: 'flex', justifyContent: 'right'}} id="wcb" class="carbonbadge .wcb-d"></div>
         </main>
     )
 }
