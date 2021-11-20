b86182ac8278ee63a3fb73eccbc16cfbef098058
dani <daniellesubject@gmail.com>
2021-11-19 19:26:45 -0500

    removing Link component from external links

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,9 +9,9 @@ import GithubIcon from "../assets/github.svg"
 const Footer = () => {
     return (
         <div>
-            <Link to="https://www.instagram.com/changingthesubject/" target="_blank"><img src={InstagramIcon} alt="instagram icon" /></Link>{" "}
-            <Link to="https://www.linkedin.com/in/danielle-subject/" target="_blank"><img src={LinkedInIcon} alt="linkedin icon" /></Link>{" "}
-            <Link to="https://github.com/dsubject/my-gatsby-blog" target="_blank"><img src={GithubIcon} alt="github icon" /></Link>
+            <a href="https://www.instagram.com/changingthesubject/" target="_blank"><img src={InstagramIcon} alt="instagram icon" /></a>{" "}
+            <a href="https://www.linkedin.com/in/danielle-subject/" target="_blank"><img src={LinkedInIcon} alt="linkedin icon" /></a>{" "}
+            <a href="https://github.com/dsubject/my-gatsby-blog" target="_blank"><img src={GithubIcon} alt="github icon" /></a>
         </div>
     )
 }
