b2131d5054b0c4a91df8dc6371595880167fd3e8
dani <daniellesubject@gmail.com>
2021-11-23 11:56:06 -0500

    removing ibm font to reduce page load size

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,10 +6,10 @@ import Footer from "../components/footer"
 const pageStyles = {
   color: 'rgba(255, 255, 255, 0.88)',
   padding: 96,
-  fontFamily: "'IBM Plex Mono', monospace",
   maxWidth: '42rem',
   marginRight: 'auto',
-  marginLeft: 'auto'
+  marginLeft: 'auto',
+  fontFamily: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
 }
 
 const Layout = ({ children }) => {
@@ -17,7 +17,6 @@ const Layout = ({ children }) => {
         <main style={pageStyles}>
             <Helmet>
                 <title>Dani Subject</title>
-                <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300&display=swap" rel="stylesheet"/>
             </Helmet>
             {children}
             <Footer />
