c0105a8b3a14084920e60baa8fb8c2d03f58490d
dani <daniellesubject@gmail.com>
2022-02-18 14:27:39 -0500

    trying to add meta image

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,9 +1,10 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Helmet } from 'react-helmet'
+import { useStaticQuery, graphql } from 'gatsby'
+import metaImage from '../assets/encryptas.png';
 
-function SEO({ description, lang, meta, image: metaImage, title, pathname }) {
+function SEO ({ description, lang, meta, image: metaImage, title, pathname }) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -27,14 +28,15 @@ function SEO({ description, lang, meta, image: metaImage, title, pathname }) {
       ? `${site.siteMetadata.siteUrl}${metaImage.src}`
       : null
 
-/* canonical link is a hint to a search engine that this is the source* for this content - it helps
-resolve duplicate content issues  */     
+  /* canonical link is a hint to a search engine that this is the source* for this content - it helps
+resolve duplicate content issues  */
+
   const canonical = pathname ? `${site.siteMetadata.siteUrl}${pathname}` : null
 
   return (
     <Helmet
       htmlAttributes={{
-        lang,
+        lang
       }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
@@ -42,71 +44,71 @@ resolve duplicate content issues  */
         canonical
           ? [
               {
-                rel: "canonical",
-                href: canonical,
-              },
+                rel: 'canonical',
+                href: canonical
+              }
             ]
           : []
       }
       meta={[
         {
           name: `description`,
-          content: metaDescription,
+          content: metaDescription
         },
         {
-          name: "keywords",
-          content: site.siteMetadata.keywords,
+          name: 'keywords',
+          content: site.siteMetadata.keywords
         },
         {
           property: `og:title`,
-          content: title,
+          content: title
         },
         {
           property: `og:description`,
-          content: metaDescription,
+          content: metaDescription
         },
         {
           property: `og:type`,
-          content: `website`,
+          content: `website`
         },
         {
           name: `twitter:creator`,
-          content: site.siteMetadata.author,
+          content: site.siteMetadata.author
         },
         {
           name: `twitter:title`,
-          content: title,
+          content: title
         },
         {
           name: `twitter:description`,
-          content: metaDescription,
-        },
+          content: metaDescription
+        }
       ]
         .concat(
           metaImage
             ? [
                 {
-                  property: "og:image",
-                  content: image,
+                  property: 'og:image',
+                  content: image
                 },
                 {
-                  property: "og:image:width",
-                  content: metaImage.width,
+                  property: 'og:image:width',
+                  content: metaImage.width
                 },
                 {
-                  property: "og:image:height",
-                  content: metaImage.height,
+                  property: 'og:image:height',
+                  content: metaImage.height
                 },
                 {
-                  name: "twitter:card",
-                  content: "summary_large_image",
-                },
+                  name: 'twitter:card',
+                  content: 'summary_large_image'
+                }
               ]
             : [
                 {
-                  name: "twitter:card",
-                  content: "summary",
-                },
+                  name: 'twitter:card',
+                  content: 'summary'
+                }
               ]
         )
         .concat(meta)}
@@ -117,7 +119,7 @@ resolve duplicate content issues  */
 SEO.defaultProps = {
   lang: `en`,
   meta: [],
-  description: ``,
+  description: ``
 }
 
 SEO.propTypes = {
@@ -128,9 +130,9 @@ SEO.propTypes = {
   image: PropTypes.shape({
     src: PropTypes.string.isRequired,
     height: PropTypes.number.isRequired,
-    width: PropTypes.number.isRequired,
+    width: PropTypes.number.isRequired
   }),
-  pathname: PropTypes.string,
+  pathname: PropTypes.string
 }
 
 export default SEO
